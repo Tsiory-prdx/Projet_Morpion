0747729535888c1657b1ff07f80ffbecbea81bef
Tsiory_prdx <tsiory.rktmhnn@gmail.com>
2019-10-18 11:16:00 +0300

    All about the players

---
+++ lib/player.rb
@@ -0,0 +1,10 @@
+class Player
+
+    attr_accessor :name, :value
+
+    def initialize(player_s_name , player_s_value)
+        @name = player_s_name
+        @value = player_s_value 
+    end
+
+end
